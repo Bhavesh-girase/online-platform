ff58ef09611ccc50c87ec3e6335d2736f78fa9cd
Bhavesh-girase <bhaveshgirase27@gmail.com>
2025-05-18 16:32:01 +0530

    add the vercel json

--- student/src/main.jsx
+++ student/src/main.jsx
@@ -1,3 +1,5 @@
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -5,7 +7,6 @@ import { AppContextProvider } from './context/AppContext.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import {ClerkProvider} from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
